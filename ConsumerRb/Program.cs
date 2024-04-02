4c4ad84fef552cda5c9874470d30655befc26c6b
ismet ertugral <ismetertugral@hotmail.com>
2024-04-02 22:43:57 +0300

    InitialCreate

---
+++ ConsumerRb/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var connectionFactory = new ConnectionFactory();
+connectionFactory.HostName = "localhost";
+var connection = connectionFactory.CreateConnection();
+var channel = connection.CreateModel();
+
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queue: queueName, exchange: "exmsg", routingKey: string.Empty);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var byteMessage = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(byteMessage);
+    channel.BasicAck(ea.DeliveryTag, multiple: false);
+    System.Console.WriteLine("Mesaj geldi: " + message);
+};
+channel.BasicConsume(queueName/*queue: "Hello"*/ , autoAck: false, consumer: consumer);
+
+Console.ReadKey();
---
+++ ProducerRb/Program.cs
@@ -0,0 +1,23 @@
+﻿//connectionFactory connection channel
+using System.Text;
+using RabbitMQ.Client;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+var connection = connectionFactory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+// channel.QueueDeclare(queue: "Hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+channel.ExchangeDeclare(exchange:"exmsg",ExchangeType.Fanout);
+var message = "Hello RabbitMQ";
+var messageBytes = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "exmsg" /*exchange: ""*/, routingKey: string.Empty /*routingKey: "Hello"*/, basicProperties: null, body: messageBytes);
+
+System.Console.WriteLine("Mesaj Gönderildi");
+
+Console.ReadKey();
